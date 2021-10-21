bae88f632c894c49a1c0cc676ef4e8a904ea3750
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2021-10-21 10:43:16 -0300

    #16 Add imagen

---
+++ resources/js/Componentes/Imagen.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-img :width="ancho" :height="alto" :src="img" :lazy-src="imglazy"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Imagen",
+    props: {
+        ancho: String,
+        alto: String,
+        img: String,
+        imglazy: String,
+    }
+}
+</script>
