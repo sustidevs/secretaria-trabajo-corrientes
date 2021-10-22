3dc72a5df7fa13c4be1ad18b312aae7839701c2f
Pruyas Araceli <pruyasaraceli@gmail.com>
2021-10-21 23:13:01 -0300

    #26 Add mapa.svg

--- resources/js/Pages/Delegaciones.vue
+++ resources/js/Pages/Delegaciones.vue
@@ -1,7 +1,16 @@
 <template>
     <layout-no-registrado>
         <Titulo class="pb-8" texto="DELEGACIONES PROVINCIALES"/>
-        <cuadro-delegaciones-areas/>
+        <mapap/>
+        <v-row>
+            <v-col cols="12" lg="6">
+                <cuadro-delegaciones-areas/>
+            </v-col>
+            <v-col cols="12" lg="6">
+                <mapa/>
+            </v-col>
+        </v-row>
+
     </layout-no-registrado>
 </template>
 
@@ -9,8 +18,9 @@
 import CuadroDelegacionesAreas from "../Componentes/CuadroDelegacionesAreas"
 import Titulo from "../Componentes/Titulo";
 import LayoutNoRegistrado from "../Layouts/LayoutNoRegistrado";
-
+import Mapa from "../Componentes/Mapa"
+import Mapap from "../Componentes/Mapap"
 export default {
-    components: {LayoutNoRegistrado, CuadroDelegacionesAreas, Titulo}
+    components: {LayoutNoRegistrado, CuadroDelegacionesAreas, Titulo, Mapa, Mapap}
 }
 </script>
