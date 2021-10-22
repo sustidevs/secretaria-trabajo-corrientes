1905792674b9e954ef167d7c7ee3e71122159cd6
Pruyas Araceli <pruyasaraceli@gmail.com>
2021-10-22 20:46:38 -0300

    #33 Add card organigrama

---
+++ resources/js/Componentes/CardOrganigrama.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card :width="width" elevation="2" color="light-green lighten-1 px-4" >
+        <v-row no-gutters justify="center">
+            <div class="MyriadPro-Cond size-org grey--text text--darken-3">
+                {{ texto }}
+            </div>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+
+export default {
+    props: {
+        width: String,
+        texto: String
+    }
+}
+
+</script>
+
+<style>
+.size-org{
+    font-size: 30px;
+}
+</style>
