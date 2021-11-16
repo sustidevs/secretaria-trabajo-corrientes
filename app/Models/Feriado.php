47e8f30bbcc8ce57c3fa4a3442b6b09504686631
FernandezJuan017 <juancruzf017@gmail.com>
2021-11-16 10:50:11 -0300

    #54 revision modelo feriado

--- app/Models/Feriado.php
+++ app/Models/Feriado.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Feriado extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
+    public static function dias_feriados(){
+        $diasFeriados = Collect([]);
+        foreach (Feriado::all() as $feriado) {
+            $fecha = Carbon::createFromDate(null, $feriado->mes, $feriado->dia);
+            $diasFeriados->push($fecha->toDateString());
+        }
+        return $diasFeriados->toArray();
+    }
+
 }
