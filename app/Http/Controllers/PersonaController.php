0137d95bf6dcddc359d94dc4af7b1f771dacf698
Juan <78928643+jNNx@users.noreply.github.com>
2021-11-17 12:40:45 -0300

    60/revision persona controller
    added index por cada tipo de persona
    *edit archivo VerifyCsfrToken porque me tira conflicto al probar las rutas post.

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Persona;
 use Illuminate\Http\Request;
 
 class PersonaController extends Controller
 {
-    //
+    public function index()
+    {
+        $personas = Persona::all();
+        return response()->json($personas, 200);
+    }
+
+    public function indexDelegados()
+    {
+        $delegados = Persona::delegados();
+        return response()->json($delegados, 200);
+    }
+
+    public function indexAdminAreas()
+    {
+        $admins = Persona::adminAreas();
+        return response()->json($admins, 200);
+    }
+
+    public function indexAbogados()
+    {
+        $abogados = Persona::abogados();
+        return response()->json($abogados, 200);
+    }
+
+    public function indexSolicitantes()
+    {
+        $solicitantes = Persona::solicitantes();
+        return response()->json($solicitantes, 200);
+    }
+
+    public function edit(Request $request)
+    {
+        $solicitante = Persona::findOrFail($request->id)->where('tipo', 7)->get();
+        return response()->json($solicitante, 200);
+    }
+    
 }
