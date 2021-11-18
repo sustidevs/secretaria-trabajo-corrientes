34e5c41f60964976a9d8c9e2efa5413c6791191a
Agustin <72084815+AgustinGM09@users.noreply.github.com>
2021-11-18 12:49:11 -0300

    añadidos

--- app/Http/Controllers/FormularioController.php
+++ app/Http/Controllers/FormularioController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\Turno;
+use App\Models\Oficina;
+use App\Models\Persona;
 use App\Models\Localidad;
 use App\Models\Delegacion;
 use Illuminate\Support\Str;
@@ -24,4 +27,185 @@ class FormularioController extends Controller
             'dataTramites'      => $tiposTramites
         ]);
     }
+
+    public function storeDatos(Request $request)
+    {
+        
+        $persona = Persona::si_existe($request->dni);
+        if ($persona == null ) {
+            $datosPersona = new Persona();
+            $datosPersona->dni = $request->dni;  
+        } 
+        else {
+            $datosPersona = $persona; 
+        }
+        $datosPersona->nombre = $request->nombre;
+        $datosPersona->apellido = $request->apellido;
+        $datosPersona->telefono = $request->telefono;
+        $datosPersona->domicilio = $request->domicilio;            
+        $datosPersona->localidad_id = $request->localidad["id"];
+        $datosPersona->correo = $request->correo;
+        $datosPersona->tipo = 7;
+        $datosPersona->save();
+        
+        if ($request->posee_abogado == true)
+        {
+            $abogado = Persona::si_existe($request->dnip);
+            if ($abogado == null ) {
+                $datosAbogado = new Persona();
+                $datosAbogado->dni = $request->dnip;  
+            } 
+            else {
+                $datosAbogado = $abogado; 
+            }
+            
+            $datosAbogado->nombre = $request->nombrep;
+            $datosAbogado->apellido = $request->apellidop;
+            $datosAbogado->telefono = $request->telefonop;
+            $datosAbogado->domicilio = $request->domiciliop;
+            $datosAbogado->localidad_id = $request->localidadp["id"];
+            $datosAbogado->correo = $request->correop;
+            $datosAbogado->tipo = 6;
+            $datosAbogado->save();
+            $dataAbogado =  $datosAbogado->datosPersona(); 
+            $tipoTramite = $request->tipo_tramite_id;
+            $delegacion= $request->delegacion_id;
+            $oficina = Oficina::getOficina($delegacion, $tipoTramite);              
+
+        }
+        else{
+            $dataAbogado = Collect([]);
+            $tipoTramite = 1;
+            $delegacion= $request->delegacion_id;
+            $oficina = Oficina::getOficina($delegacion, $tipoTramite);  
+        }
+                
+        $fechasDisponibles = Oficina::findOrFail($oficina->id)->array_fechas_horarios(); 
+        $dataTurno = Collect([]);
+
+        /* Pregunta si el solicitante ya tiene un turno pendiente para ese tramite */
+        
+        $verifica_turno = Turno::verifica_si_existe($datosPersona->id,$oficina->id);
+        if ($verifica_turno->count() > 1) {
+            $dataTurno = $verifica_turno->first()->datosTurno();
+            //dd ($dataTurno);
+            return Inertia::render('FormularioTurno/Formulario', 
+            [   
+                                'oficina_id' => $oficina->id,
+                                'delegacion' => $delegacion,
+                                'tipoTramite' => $tipoTramite,
+                                'dataFechas' => $fechasDisponibles, 
+                                'dataSolicitante' => $datosPersona->datosPersona(),
+                                'dataAbogado' => $dataAbogado,
+                                'dataTurno' => $dataTurno,
+                                'verifica_turno' => true,
+                                
+            ]);
+        } 
+        else {
+            return Inertia::render('FormularioTurno/FechasDisponibles', 
+            [   
+                                'oficina_id' => $oficina->id,
+                                'delegacion' => $delegacion,
+                                'tipoTramite' => $tipoTramite,
+                                'dataFechas' => $fechasDisponibles, 
+                                'dataSolicitante' => $datosPersona->datosPersona(),
+                                'dataAbogado' => $dataAbogado,
+                                'dataTurno' => $dataTurno,
+                                'verifica_turno' => false,
+                                
+            ]);
+        }  
+    }
+
+    public function storeTurnos(Request $request)
+    {
+        
+        $persona = Persona::si_existe($request->dni);
+        if ($persona == null ) {
+            $datosPersona = new Persona();
+            $datosPersona->dni = $request->dni;  
+        } 
+        else {
+            $datosPersona = $persona; 
+        }
+        $datosPersona->nombre = $request->nombre;
+        $datosPersona->apellido = $request->apellido;
+        $datosPersona->telefono = $request->telefono;
+        $datosPersona->domicilio = $request->domicilio;            
+        $datosPersona->localidad_id = $request->localidad["id"];
+        $datosPersona->correo = $request->correo;
+        $datosPersona->tipo = 7;
+        $datosPersona->save();
+        
+        if ($request->posee_abogado == true)
+        {
+            $abogado = Persona::si_existe($request->dnip);
+            if ($abogado == null ) {
+                $datosAbogado = new Persona();
+                $datosAbogado->dni = $request->dnip;  
+            } 
+            else {
+                $datosAbogado = $abogado; 
+            }
+            
+            $datosAbogado->nombre = $request->nombrep;
+            $datosAbogado->apellido = $request->apellidop;
+            $datosAbogado->telefono = $request->telefonop;
+            $datosAbogado->domicilio = $request->domiciliop;
+            $datosAbogado->localidad_id = $request->localidadp["id"];
+            $datosAbogado->correo = $request->correop;
+            $datosAbogado->tipo = 6;
+            $datosAbogado->save();
+            $dataAbogado =  $datosAbogado->datosPersona(); 
+            $tipoTramite = $request->tipo_tramite_id;
+            $delegacion= $request->delegacion_id;
+            $oficina = Oficina::getOficina($delegacion, $tipoTramite);              
+
+        }
+        else{
+            $dataAbogado = Collect([]);
+            $tipoTramite = 1;
+            $delegacion= $request->delegacion_id;
+            $oficina = Oficina::getOficina($delegacion, $tipoTramite);  
+        }
+                
+        $fechasDisponibles = Oficina::findOrFail($oficina->id)->array_fechas_horarios(); 
+        $dataTurno = Collect([]);
+
+        /* Pregunta si el solicitante ya tiene un turno pendiente para ese tramite */
+        
+        $verifica_turno = Turno::verifica_si_existe($datosPersona->id,$oficina->id);
+        if ($verifica_turno->count() > 1) {
+            $dataTurno = $verifica_turno->first()->datosTurno();
+            //dd ($dataTurno);
+            return Inertia::render('FormularioTurno/Formulario', 
+            [   
+                                'oficina_id' => $oficina->id,
+                                'delegacion' => $delegacion,
+                                'tipoTramite' => $tipoTramite,
+                                'dataFechas' => $fechasDisponibles, 
+                                'dataSolicitante' => $datosPersona->datosPersona(),
+                                'dataAbogado' => $dataAbogado,
+                                'dataTurno' => $dataTurno,
+                                'verifica_turno' => true,
+                                
+            ]);
+        } 
+        else {
+            return Inertia::render('FormularioTurno/FechasDisponibles', 
+            [   
+                                'oficina_id' => $oficina->id,
+                                'delegacion' => $delegacion,
+                                'tipoTramite' => $tipoTramite,
+                                'dataFechas' => $fechasDisponibles, 
+                                'dataSolicitante' => $datosPersona->datosPersona(),
+                                'dataAbogado' => $dataAbogado,
+                                'dataTurno' => $dataTurno,
+                                'verifica_turno' => false,
+                                
+            ]);
+        } 
+    }
+    
 }
