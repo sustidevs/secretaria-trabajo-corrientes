993a5cf4361e0d76c3d2fa93bef08a9628652d70
marianoflowers <mariano.alfredo.flores@gmail.com>
2021-11-25 10:02:01 -0300

    #67 Fix LogoutController

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
+class LogoutController extends Controller
 {
     public function logout(Request $request)
     {
