be171378ddb9cbfba0d15f2fddb17f0ddaf533c9
Pruyas Araceli <pruyasaraceli@gmail.com>
2021-12-02 21:14:39 -0300

    #68 tasas

--- resources/js/Layouts/Componentes/NavbarNoRegistrado.vue
+++ resources/js/Layouts/Componentes/NavbarNoRegistrado.vue
@@ -6,7 +6,7 @@
                  <a href="/inspectores"><dropdown-list  titulo="Inspectores"/></a>
                  <a href="/tramites"><dropdown-list  titulo="Tramites"/></a>
                 <a href="/app-trabajo"><dropdown-list  titulo="App trabajo"/></a>
-                <a href="/"><dropdown-list  titulo="Pago Tasas"/></a>
+                <a href="/tasas"><dropdown-list  titulo="Pago Tasas"/></a>
                 <v-row justify="end" class="px-6">
                     <a href="https://twitter.com/setracorr?s=08" target="blank" ><img width="25" class="mr-8" height="25" :src = "'/images/twitter.svg'" alt=""></a>
                     <a href="https://www.facebook.com/SecretariaDeTrabajoCorrientes/" target="blank"> <img width="11" height="11" v-bind:src = "'/images/facebook.svg'" alt=""></a>
---
+++ resources/js/Pages/PagoTasas.vue
@@ -0,0 +1,35 @@
+<template>
+    <layout-no-registrado>
+        <titulo-tramite texto="Tasas de la Secretaría de Trabajo"/>
+        <descripcion texto="A partir de ahora se podrán abonar multas y otras tasas de la Secretaría de Trabajo por medio de Red Link, Link Celular."/>
+
+        <v-row justify="center">
+            <v-col justify="center" align="center" cols="12" sm="12" md="12" lg="6">
+                <img class="pa-2" :src="('./images/tasas-ya.jpg')">
+            </v-col>
+            <v-col cols="12" sm="12" md="12" lg="6" justify="center" align="center">
+                <video :poster="('./images/tasas.jpg')" :src="('./videos/tasastrabajo.mp4')" width="640" height="480" controls></video>
+            </v-col>
+        </v-row>
+
+    </layout-no-registrado>
+</template>
+
+<script>
+import LayoutNoRegistrado from "../Layouts/LayoutNoRegistrado";
+import TituloTramite from '../Componentes/TituloTramite'
+import Descripcion from '../Componentes/Descripcion';
+
+export default {
+    components: {LayoutNoRegistrado,  TituloTramite, Descripcion},
+
+    data: () => ({
+        selectedItem: 1,
+        items: [
+            { text: 'Optimiza las inspecciones laborales que se realicen en el ambito de la provincia', icon: 'mdi-check-bold' },
+            { text: 'Pueden utilizar todos los trabajados e incluso cualquier ciudadano que observe anomalias en los distintos lugares de trabajo', icon: 'mdi-check-bold' },
+            { text: 'Practico y sencillo', icon: 'mdi-check-bold' },
+        ],
+    }),
+}
+</script>
